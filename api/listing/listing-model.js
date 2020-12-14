45cf7a7cea107163e0c272f4239adec27a3d2b70
tucker <tuckerwray84@gmail.com>
2020-12-14 14:12:32 -0500

    feat- Listing search amenities - new model function retreives listings by amenities passed to reqeust.body - new route allows acces to listings by amenity at listing/searchlistings

--- api/listing/listing-model.js
+++ api/listing/listing-model.js
@@ -1,4 +1,4 @@
-const db = require('../../data/dbConfig.js')
+const db = require("../../data/dbConfig.js");
 
 // 'landowner' & 'listing'
 
@@ -6,7 +6,7 @@ module.exports = {
   add,
   addReserve,
   addlisting,
-
+  findListingsByQuery,
   find,
   findBy,
   findById,
@@ -14,153 +14,148 @@ module.exports = {
   findTableBy,
   remove,
   update,
-  updateById
-}
+  updateById,
+};
 
-function add (newinsert, tableprop) {
-
-  const table = tableprop.toString()
-  console.log('newlistingmodel:', newinsert)
-  console.error()
+function add(newinsert, tableprop) {
+  const table = tableprop.toString();
+  console.log("newlistingmodel:", newinsert);
+  console.error();
   return db(table)
-    .insert(newinsert, 'id')
+    .insert(newinsert, "id")
     .then(([id]) => {
-      console.log('listingid-afterAdd\'s-Findby', id)
-      console.error()
-      return findById(id)
-    })
+      console.log("listingid-afterAdd's-Findby", id);
+      console.error();
+      return findById(id);
+    });
 }
-function addReserve (reserve, tableprop) {
-  const table = tableprop.toString()
+function addReserve(reserve, tableprop) {
+  const table = tableprop.toString();
   return db(table)
-    .insert(reserve, 'id')
+    .insert(reserve, "id")
     .then(([id]) => {
-      return db(table).where('id', id)
-    })
+      return db(table).where("id", id);
+    });
 }
 
-function addlisting (tableprop, newaddition, landowner_id) {
-  const table = tableprop.toString()
+function addlisting(tableprop, newaddition, landowner_id) {
+  const table = tableprop.toString();
   return db(table)
     .insert({ ...newaddition, landowner_id })
     .then(([id]) => {
-      return db(table).where({ id })
-    })
+      return db(table).where({ id });
+    });
 }
 
-// function addTask (task, landowner_id) {
-//   return db('tasks')
-//     .insert({ ...task, landowner_id })
-//     .then(([id]) => {
-//       return db('tasks').where({ id })
-//     })
-// }
-
-// array of all landowner
-function find (prop) {
-  const table = prop.toString()
-  return db(table)
+function find(prop) {
+  const table = prop.toString();
+  return db(table);
+}
+function findListingsByQuery(query) {
+  return db("amenities").where(query);
+  {
+    /**
+  query is an object of key value pairs 
+  the where(~mixed~) syntax breaks the query object down
+   into "where a and b and c.." constraints
+  knex.where({k:v,k2:v2,k3:v3})===where k=v and k2=v2 and k3=v3 
+*/
+  }
 }
 
-function findBy (prop, filter) {
-  const table = prop.toString()
-  return db(table).where('id', filter)
+function findBy(prop, filter) {
+  const table = prop.toString();
+  return db(table).where("id", filter);
 }
 
-function findById (id) {
-  console.log(id)
-  return db('listing')
-    .where('id', id)
-    .first()
+function findById(id) {
+  console.log(id);
+  return db("listing").where("id", id).first();
 }
 
-function findTableBy (prop, filter) {
-  const table = prop.toString()
-  return db(table).where('landower_id', filter)
+function findTableBy(prop, filter) {
+  const table = prop.toString();
+  return db(table).where("landower_id", filter);
 }
 
-function findOwnerListings (id) {
-  return db('landowner')
+function findOwnerListings(id) {
+  return db("landowner")
     .join(
-      'landowner_listings',
-      'landowner.id',
-      'landowner_listings.landowner_id'
+      "landowner_listings",
+      "landowner.id",
+      "landowner_listings.landowner_id"
     )
-    .join('listing', 'listing.landowner_id', 'landowner_listings.landowner_id')
-    .join('landowner', 'landowner_listings.landowner_id', '')
+    .join("listing", "listing.landowner_id", "landowner_listings.landowner_id")
+    .join("landowner", "landowner_listings.landowner_id", "")
     .select(
-      'listing.id',
-      'landowner.username',
-      'listing.price',
-      'listing.location',
-      'listing.amenities'
+      "listing.id",
+      "landowner.username",
+      "listing.price",
+      "listing.location",
+      "listing.amenities"
     )
-    .where('listing.landowner_id', id)
-    .orderBy('tasks.id', 'asc')
+    .where("listing.landowner_id", id)
+    .orderBy("tasks.id", "asc");
 }
-function findReservationsByRv (id) {
-  return db('tasks ')
-    .join('landowner', 'landowner.id', 'tasks.landowner_id')
+function findReservationsByRv(id) {
+  return db("tasks")
+    .join("landowner", "landowner.id", "tasks.landowner_id")
     .select(
-      'tasks.id',
-      'landowner.landowner_name',
-      'tasks.id',
-      'tasks.notes',
-      'tasks.description',
-      'tasks.completed'
+      "tasks.id",
+      "landowner.landowner_name",
+      "tasks.id",
+      "tasks.notes",
+      "tasks.description",
+      "tasks.completed"
     )
-    .where('tasks.landowner_id', id)
-    .orderBy('tasks.id', 'asc')
+    .where("tasks.landowner_id", id)
+    .orderBy("tasks.id", "asc");
 }
-function findReservationsByListing (id) {
-  return db('tasks ')
-    .join('landowner', 'landowner.id', 'tasks.landowner_id')
+function findReservationsByListing(id) {
+  return db("tasks")
+    .join("landowner", "landowner.id", "tasks.landowner_id")
     .select(
-      'tasks.id',
-      'landowner.landowner_name',
-      'tasks.id',
-      'tasks.notes',
-      'tasks.description',
-      'tasks.completed'
+      "tasks.id",
+      "landowner.landowner_name",
+      "tasks.id",
+      "tasks.notes",
+      "tasks.description",
+      "tasks.completed"
     )
-    .where('tasks.landowner_id', id)
-    .orderBy('tasks.id', 'asc')
+    .where("tasks.landowner_id", id)
+    .orderBy("tasks.id", "asc");
 }
-function findReservationsByOwner (id) {
-  return db('listing')
-    .join('landowner', 'landowner.id', 'listing.landowner_id')
+function findReservationsByOwner(id) {
+  return db("listing")
+    .join("landowner", "landowner.id", "listing.landowner_id")
     .select(
-      'tasks.id',
-      'landowner.landowner_name',
-      'tasks.id',
-      'tasks.notes',
-      'tasks.description',
-      'tasks.completed'
+      "tasks.id",
+      "landowner.landowner_name",
+      "tasks.id",
+      "tasks.notes",
+      "tasks.description",
+      "tasks.completed"
     )
-    .where('tasks.landowner_id', id)
-    .orderBy('tasks.id', 'asc')
+    .where("tasks.landowner_id", id)
+    .orderBy("tasks.id", "asc");
 }
 
-function remove (prop, id) {
-  const table = prop.toString()
-  return db(table)
-    .where('id', id)
-    .del()
+function remove(prop, id) {
+  const table = prop.toString();
+  return db(table).where("id", id).del();
 }
 
-function update (id, changes, prop) {
-  const table = prop.toString()
-  return db(table)
-    .where({ id })
-    .update(changes, '*')
+function update(id, changes, prop) {
+  const table = prop.toString();
+  return db(table).where({ id }).update(changes, "*");
 }
 
-function updateById (changes, id, prop) {
-  const table = prop.toString()
-  console.log('id', id)
-  const listID = Number(id)
-  console.log('listID', listID)
-  return findBy(table, id).then(r => r.update(changes))
+function updateById(changes, id, prop) {
+  const table = prop.toString();
+  console.log("id", id);
+  const listID = Number(id);
+  console.log("listID", listID);
+  return findBy(table, id).then((r) => r.update(changes));
   // .then(()=>findBy('listing',id))
 }
 
--- api/listing/listing-router.js
+++ api/listing/listing-router.js
@@ -1,6 +1,6 @@
-const db = require('../../data/dbConfig.js')
-const list = require('./listing-model.js')
-const router = require('express').Router()
+const db = require("../../data/dbConfig.js");
+const list = require("./listing-model.js");
+const router = require("express").Router();
 // //MVP
 // 1. Users can register/create an account as either an **RV Owner** or a **Landowner** (web, mobile)
 // 2. **Landowners** and **RV Owners** can login to the the app. (web, mobile)
@@ -13,88 +13,113 @@ const router = require('express').Router()
 /// rv owners need to  view  and deleted all reservations for the listings by date availability
 /// rv owners need to create view update and deleted single fav reservations single reservations for the listings by date availability
 
-router.get('/', async (req, res) => {
+router.get("/searchlistings", async (req, res) => {
+  console.log("req.BODY", req.body);
   list
-    .find('listing')
-    .then(listings => {
+    .findListingsByQuery(req.body)
+    .then((listings) =>
       listings.length === undefined
-        ? res.status(404).json({ message: 'the listings could not be found' })
+        ? res.status(404).json({ message: "the listings could not be found" })
         : listings.length === 0
-          ? res.status(200).json({ message: 'No listings. ' })
-          : res.status(200).json({ listings })
-    })
-    .catch(error => {
+        ? res
+            .status(200)
+            .json({ message: "No Listings matching these criteria." })
+        : res.status(200).json({ listings })
+    )
+    .catch((error) => {
       res.status(500).json({
-        error: `there was an error on the server while attempting to retrieve the listings.--+${error}`
-      })
-      console.error(error)
+        error: `there was an error on the server while attempting to retrieve listings of the selected criteria.--${error}`,
+      });
+      console.error(error);
+    });
+});
+
+router.get("/", async (req, res) => {
+  list
+    .find("listing")
+    .then((listings) => {
+      listings.length === undefined
+        ? res.status(404).json({ message: "the listings could not be found" })
+        : listings.length === 0
+        ? res.status(200).json({ message: "No listings. " })
+        : res.status(200).json({ listings });
     })
-})
+    .catch((error) => {
+      res.status(500).json({
+        error: `there was an error on the server while attempting to retrieve the listings.--+${error}`,
+      });
+      console.error(error);
+    });
+});
 
 // hardcode listing by req.params.id
 // this can return listings by owner or listing id
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   list
-    .findBy('listing', req.params.id)
-    .then(singleListing => {
+    .findBy("listing", req.params.id)
+    .then((singleListing) => {
       singleListing.length == 0 || undefined
-        ? res.status(404).json({ message: 'the listing could not be found' })
-        : res.status(200).json({ singleListing })
+        ? res.status(404).json({ message: "the listing could not be found" })
+        : res.status(200).json({ singleListing });
     })
-    .catch(error => {
-      res.status(500).json({ message: error })
-      console.error(error)
-    })
-})
+    .catch((error) => {
+      res.status(500).json({ message: error });
+      console.error(error);
+    });
+});
 
 // hardcode add new listing by req.body
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   list
-    .add(req.body, 'listing')
-    .then(listingUp => {
-      res.status(201).json({ 'newlisting added': listingUp })
+    .add(req.body, "listing")
+    .then((listingUp) => {
+      res.status(201).json({ "newlisting added": listingUp });
     })
-    .catch(error => {
-      res.status(500).json({ error: `that didnt work my friend||${console.error(error)}||--${error}--||` })
-      console.error(error)
-    })
-})
+    .catch((error) => {
+      res.status(500).json({
+        error: `that didnt work my friend||${console.error(
+          error
+        )}||--${error}--||`,
+      });
+      console.error(error);
+    });
+});
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   list
-    .update(req.params.id, req.body, 'listing')
-    .then(updatedListing => {
+    .update(req.params.id, req.body, "listing")
+    .then((updatedListing) => {
       updatedListing.length == 0 || undefined
-        ? res.status(404).json({ message: 'the listing could not be found' })
+        ? res.status(404).json({ message: "the listing could not be found" })
         : res
-          .status(200)
-          .json({ message: 'Listing updated successfully', updatedListing })
+            .status(200)
+            .json({ message: "Listing updated successfully", updatedListing });
     })
-    .catch(error => {
-      console.log('PUT /api/user/:id Error', error)
+    .catch((error) => {
+      console.log("PUT /api/user/:id Error", error);
       res
         .status(500)
-        .json({ error: 'We ran into an error updating the listing' })
-    })
-})
+        .json({ error: "We ran into an error updating the listing" });
+    });
+});
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   list
-    .remove('listing', req.params.id)
-    .then(count => {
+    .remove("listing", req.params.id)
+    .then((count) => {
       if (count > 0) {
-        res.status(200).json({ message: 'The listing has been nuked' })
+        res.status(200).json({ message: "The listing has been nuked" });
       } else {
-        res.status(404).json({ message: 'The listing could not be found' })
+        res.status(404).json({ message: "The listing could not be found" });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       // log error to server
-      console.log(error)
+      console.log(error);
       res.status(500).json({
-        message: 'Error removing the listing'
-      })
-    })
-})
+        message: "Error removing the listing",
+      });
+    });
+});
 
-module.exports = router
+module.exports = router;
